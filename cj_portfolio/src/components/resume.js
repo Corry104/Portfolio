a27b8e4d4c8b0847fbf2fa47a648767c60dbfd68
Corrado Alfano <corrado.alfano10@gmail.com>
2019-07-12 14:52:43 -0700

    resume updates

--- cj_portfolio/src/components/resume.js
+++ cj_portfolio/src/components/resume.js
@@ -38,10 +38,10 @@ class Resume extends Component {
                         <h5>Email</h5>
                         <a href="mailto:cj.alfano@yahoo.com?subject=We%20want%20you%20in%20our%20team!!&body=message%20goes%20here"><strong>cj.alfano@yahoo.com</strong></a>
                         <h5>Resume</h5>
-                        <a href='https://drive.google.com/file/d/1V8-VfaZZgYF_fVsrlsaSvuGXyvhfTERF/view?usp=sharing' target='a_blank'><strong>Resume Colored Version</strong></a>
+                        <a href='https://drive.google.com/file/d/1cAGn_S11D7szywPWg_nMfrm7C6x-gKJl/view?usp=sharing' target='a_blank'><strong>Resume Colored Version</strong></a>
                         <br/>
                         <br/>
-                        <a href='https://docs.google.com/document/d/1Wd9jdQPf1nBYOpDU1xY4NgMidM9JDtLLWo8S2lD0B78/edit?usp=sharing' target='a_blank'><strong>Resume Black & White Version</strong></a>
+                        <a href='https://drive.google.com/file/d/1k5K4oyIx7QZAne5MEd3DS-7LHAxlMpGE/view?usp=sharing' target='a_blank'><strong>Resume Black & White Version</strong></a>
 
                         <h5>Languages Spoken</h5>
                         <p><span role="img" aria-label="british-flag">🇬🇧 English</span></p>
